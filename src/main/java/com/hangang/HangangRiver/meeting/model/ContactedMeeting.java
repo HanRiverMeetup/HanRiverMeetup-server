b8ef644c26e2bfd8019d1d94b3eebdb229f9b826
Taerry <kimts2002kr@naver.com>
2018-07-31 22:03:20 +0900

    Revert "Release"

--- src/main/java/com/hangang/HangangRiver/meeting/model/ContactedMeeting.java
+++ src/main/java/com/hangang/HangangRiver/meeting/model/ContactedMeeting.java
@@ -2,13 +2,10 @@ package com.hangang.HangangRiver.meeting.model;
 
 import java.util.Date;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 public class ContactedMeeting {
     private int contact_seq;
     private int meeting_seq;
     private int application_seq;
-    @JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss" ,timezone="Asia/Seoul")
     private Date contact_time;
     private String host_user_id;
     private String guest_user_id;
